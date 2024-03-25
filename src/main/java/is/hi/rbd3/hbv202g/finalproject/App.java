9e09ae0c6939b412a9a0500fc6a91a86fba2bf55
Lumarochoco <romain.degrace@gmail.com>
2024-03-25 14:36:35 +0000

    initial commit

---
+++ src/main/java/is/hi/rbd3/hbv202g/finalproject/App.java
@@ -0,0 +1,13 @@
+package is.hi.rbd3.hbv202g.finalproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/is/hi/rbd3/hbv202g/finalproject/AppTest.java
@@ -0,0 +1,20 @@
+package is.hi.rbd3.hbv202g.finalproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
