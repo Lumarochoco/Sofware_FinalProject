3ed8b7d72266bc0ed53da1d1b345a9ae6ff7c975
ChloePerrichon <chloedieffenthal@gmail.com>
2024-03-25 15:11:48 +0000

    abstract class Pokemon created

--- src/main/java/is/hi/rbd3/hbv202g/finalproject/Pokemon.java
+++ src/main/java/is/hi/rbd3/hbv202g/finalproject/Pokemon.java
@@ -1,4 +1,36 @@
 package is.hi.rbd3.hbv202g.finalproject;
 
-public class Pokemon {
+abstract class Pokemon {
+
+    private String name;
+
+    private int life;
+
+    public Pokemon(String name, int life) {
+        this.name = name;
+        this.life = life;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getLife() {
+        return this.life;
+    }
+
+    public abstract void closeAttack();
+
+    public abstract void specialAttack();
+
+    public abstract void farAttack();
+
 }
