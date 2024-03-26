c640237b11075eab6cdcb263f109e9ffc333fe78
Lumarochoco <romain.degrace@gmail.com>
2024-03-26 13:45:37 +0000

    Javadoc Combat and CombatTest

--- src/main/java/is/hi/rbd3/hbv202g/finalproject/Combat.java
+++ src/main/java/is/hi/rbd3/hbv202g/finalproject/Combat.java
@@ -3,6 +3,10 @@ package is.hi.rbd3.hbv202g.finalproject;
 import java.lang.Math;
 import java.util.Scanner;
 
+/**
+ * Methods related to manage the fight between two Pokemon.
+ */
+
 public class Combat extends Observable {
 
     private Pokemon playerPokemon;
@@ -11,6 +15,11 @@ public class Combat extends Observable {
 
     private Scanner scanner;
 
+    /**
+     * Creates a new combat object.
+     * @param playerPokemon
+     * @param boss
+     */
     public Combat (Pokemon playerPokemon, Pokemon boss){
         super();
         this.playerPokemon = playerPokemon;
@@ -20,6 +29,10 @@ public class Combat extends Observable {
         this.attach(this.boss);
     }
 
+
+    /**
+     * Runs the entire fight.
+     */
     public void match(){
         System.out.printf("-------------------------------------\n");
         System.out.println("Let's start the fight ! ");
@@ -40,6 +53,13 @@ public class Combat extends Observable {
         combatOver();
     }
 
+
+    /**
+     * Executes the play of the player by choosing an attack.
+     * 1 = close attack
+     * 2 = far attack
+     * 3 = special attack
+     */
     public void playerTurn(){
         System.out.println("It's your turn ! \n");
         System.out.printf("Choose an attack ! \n");
@@ -69,6 +89,11 @@ public class Combat extends Observable {
         System.out.println("---------------------------------");
     }
 
+    /**
+     * Executes the play of the NPC.
+     * NPC = Non-Player Caracter
+     * A random number is created to select the attack of the boss
+     */
     public void npcTurn(){
         System.out.println("It's the NPC's turn ! \n");
 
@@ -93,6 +118,9 @@ public class Combat extends Observable {
         System.out.println("---------------------------------");
     }
 
+    /**
+     * Executes the close attack of the pokemon's player and notifies the observer.
+     */
     public void playercloseAttack(){
         int damage = (int) Math.ceil(playerPokemon.closeAttack()*(Math.random() * 0.9 + 0.1));
         boss.setLife(boss.getLife()- damage);
@@ -100,6 +128,9 @@ public class Combat extends Observable {
         notifyObservers();
     }
 
+    /**
+     * Executes the far attack of the pokemon's player and notifies the observer.
+     */
     public void playerfarAttack(){
         int damage = (int) Math.ceil(playerPokemon.farAttack()*(Math.random() * 0.9 + 0.1));
         boss.setLife(boss.getLife()- damage);
@@ -107,12 +138,18 @@ public class Combat extends Observable {
         notifyObservers();
     }
 
+    /**
+     * Executes the special attack of the pokemon's player and notifies the observer.
+     */
     public void playerSpecialAttack(){
         boss.setLife(boss.getLife()- playerPokemon.specialAttack());
         System.out.println("Provocated damage : "+playerPokemon.specialAttack());
         notifyObservers();
     }
 
+    /**
+     * Executes the close attack of the boss and notofies the observer.
+     */
     public void bosscloseAttack(){
         int damage = (int) Math.ceil(boss.closeAttack()*(Math.random() * 0.9 + 0.1));
         playerPokemon.setLife(playerPokemon.getLife()- damage);
@@ -120,6 +157,9 @@ public class Combat extends Observable {
         notifyObservers();
     }
 
+    /**
+     * Executes the far attack of the boss and notofies the observer.
+     */
     public void bossfarAttack(){
         int damage = (int) Math.ceil(boss.farAttack()*(Math.random() * 0.9 + 0.1));
         playerPokemon.setLife(playerPokemon.getLife()- damage);
@@ -127,12 +167,20 @@ public class Combat extends Observable {
         notifyObservers();
     }
 
+    /**
+     * Executes the special attack of the boss and notifies the obsever.
+     */
     public void bossSpecialAttack(){
         playerPokemon.setLife(playerPokemon.getLife()- boss.specialAttack());
         System.out.println("Provocated damage : "+boss.specialAttack());
         notifyObservers();
     }
 
+    /**
+     * Checks if the fight is over.
+     * Over = false
+     * @return a boolean variable
+     */
     public boolean checkCombatOver(){
         if ((this.playerPokemon.getLife() <=0) || (this.boss.getLife()) <= 0 ){
             return false;
@@ -141,12 +189,18 @@ public class Combat extends Observable {
         }
     }
 
+    /**
+     * Writes into the terminal the winner's name.
+     */
     public void combatOver(){
         System.out.println("The fight is over ! \n");
         System.out.println("The winner is " + getNameWinner()+ "!");
     }
 
-
+    /**
+     *Gives the name of the winner of the fight.
+     * @return the name of the winner
+     */
     public String getNameWinner(){
         if(this.playerPokemon.getLife() <=0){
             return boss.getName();
@@ -155,28 +209,52 @@ public class Combat extends Observable {
         }
     }
 
-
+    /**
+     * Returns the pokemon's player.
+     * @return the pokemon's player
+     */
     public Pokemon getPlayerPokemon() {
         return playerPokemon;
     }
 
+    /**
+     * Sets the pokemon of the player.
+     * @param playerPokemon
+     */
     public void setPlayerPokemon(Pokemon playerPokemon) {
         this.playerPokemon = playerPokemon;
     }
 
+    /**
+     * Returns the boss.
+     * @return the boss pokemon
+     */
     public Pokemon getBoss() {
         return boss;
     }
 
+    /**
+     * Sets the boss pokemon.
+     * @param boss
+     */
     public void setBoss(Pokemon boss) {
         this.boss = boss;
     }
 
+    /**
+     * Returns the scanner.
+     * @return scanner
+     */
     public Scanner getScanner() {
         return scanner;
     }
 
+    /**
+     * Sets the scanner.
+     * @param scanner
+     */
     public void setScanner(Scanner scanner) {
         this.scanner = scanner;
     }
 }
+
--- src/test/java/is/hi/rbd3/hbv202g/finalproject/CombatTest.java
+++ src/test/java/is/hi/rbd3/hbv202g/finalproject/CombatTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Methods related to test methodes used to manage the fight between two Pokemon.
+ */
 public class CombatTest {
 
     private final int life = 100;
@@ -14,6 +17,9 @@ public class CombatTest {
 
     private Combat combat;
 
+    /**
+     * Creates two Pokemon and a combat object.
+     */
     @Before
     public void creationPokemonAndCombat(){
         this.pokemon = new WaterPokemon("Morice", this.life);
@@ -22,18 +28,27 @@ public class CombatTest {
         this.combat = new Combat(this.pokemon, this.boss);
     }
 
+    /**
+     * Tests if the combat is over when the player wins.
+     */
     @Test
     public void combatIsOverPlayerWins(){
         this.boss.setLife(-10);
         assertFalse(this.combat.checkCombatOver());
     }
 
+    /**
+     * Tests if the combat is over when the boss wins.
+     */
     @Test
     public void combatIsOverBossWins(){
         this.pokemon.setLife(0);
         assertFalse(this.combat.checkCombatOver());
     }
 
+    /**
+     * Tests if we get the name of the winner when the player wins.
+     */
     @Test
     public void playerWins(){
         this.pokemon.setLife(89);
@@ -42,6 +57,9 @@ public class CombatTest {
         assertEquals(this.pokemon.getName(),this.combat.getNameWinner());
     }
 
+    /**
+     * Tests if we get the name of the winner when the boss wins.
+     */
     @Test
     public void bossWins(){
         this.pokemon.setLife(0);
@@ -50,6 +68,9 @@ public class CombatTest {
         assertEquals(this.boss.getName(),this.combat.getNameWinner());
     }
 
+    /**
+     * Tests to verify that the combat is not over.
+     */
     @Test
     public void combatNotOver(){
         this.pokemon.setLife(50);
@@ -57,6 +78,18 @@ public class CombatTest {
 
         assertTrue(this.combat.checkCombatOver());
     }
+
+    /**
+     * Tests the special attack of the boss.
+     */
+    @Test
+    public void bossSpecialAttack(){
+        this.pokemon.setLife(life);
+        this.combat.bossSpecialAttack();
+
+        assertEquals(55,this.pokemon.getLife());
+    }
+
 
 
 }
